d3bb59582328d195bee379f2acd374cb6f19fa13
中里南 <minami.rage@gmail.com>
2019-12-05 18:18:59 +0900

    add : 商品テーブル(最低限)

--- db/migrate/20191205041857_create_products.rb
+++ db/migrate/20191205041857_create_products.rb
@@ -2,11 +2,11 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       t.string :name,  null: false
-      t.int :deliver_how,null: false
+      t.integer :deliver_how,null: false
       t.date :deliver_day, null: false
-      t.int :price ,null: false
+      t.integer :price ,null: false
       t.string :explanation ,null: true
-      t.int :state ,null: false
+      t.integer :state ,null: false
       t.timestamps
     end
   end
