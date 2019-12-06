5a425aeaa1f4d38b10ec8f39ed1f418ee90de766
kantashimizu <gantaiqingshui968@gmail.com>
2019-12-06 14:34:35 +0900

    add : slide2

--- app/assets/javascripts/slick.js
+++ app/assets/javascripts/slick.js
@@ -3,6 +3,7 @@ $(function() {
       dots: true,
       autoplay: true,
       autoplaySpeed: 1000,
+      arrows: true,
   });
   $('.slick-dots li').on('click', function() {
     $('.slide_image').slick('goTo', $(this).index());
