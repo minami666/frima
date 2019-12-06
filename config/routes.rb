99a3a0ce2b025a92ede192986642d26c2900e40a
中里南 <minami.rage@gmail.com>
2019-12-06 20:01:01 +0900

    fix : routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'products#index'
-  resources :products, only: [:show]
+  resources :products, only: [:index, :show]
   #collection do #検索機能用
   # get 'search'
   #end
