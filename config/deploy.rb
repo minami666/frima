b15d28c5917ca73ceafa5dc9e415160f8b30b79c
中里南 <minami.rage@gmail.com>
2019-12-07 19:33:00 +0900

    add : deployファイル追記

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,8 @@ set :application, 'frima'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:minami666/frima.git'
 
+set :branch, 'master'
+
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
