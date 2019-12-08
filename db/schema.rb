c80736f7692bbfab4836d18c5dfd8c7f1e040e5b
中里南 <minami.rage@gmail.com>
2019-12-08 16:42:20 +0900

    住所モデル&テーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_060013) do
+ActiveRecord::Schema.define(version: 2019_12_08_071042) do
+
+  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "postnum", null: false
+    t.string "prefecture", null: false
+    t.string "city", null: false
+    t.integer "street_num", null: false
+    t.string "building"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
