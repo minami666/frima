15741fbbdf8d2a640a1b5a5d06f44ef60b806a7b
中里南 <minami.rage@gmail.com>
2019-12-08 20:53:45 +0900

    fix : 評価テーブルの初期値を設定

---
+++ db/migrate/20191208115245_change_column_to_buyerevaluate.rb
@@ -0,0 +1,7 @@
+class ChangeColumnToBuyerevaluate < ActiveRecord::Migration[5.2]
+  def change
+    change_column :buyer_evaluates, :good, :integer, default: 0
+    change_column :buyer_evaluates, :ordinary, :integer, default: 0
+    change_column :buyer_evaluates, :bad, :integer, default: 0
+  end
+end
