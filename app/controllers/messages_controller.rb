1d90d0ca4a9fa40c91b0097fb74a659809906029
中里南 <minami.rage@gmail.com>
2019-12-08 22:56:32 +0900

    add : メッセージCのdef~end

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,21 @@
 class MessagesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
 end
