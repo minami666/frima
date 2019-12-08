5274a075b634c878a802e971b158ee822a721c0b
中里南 <minami.rage@gmail.com>
2019-12-08 22:52:08 +0900

    add : マイページC : def〜end追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,57 +1,76 @@
 class UsersController < ApplicationController
 
+  # マイページ／一覧
   def index
 
   end
 
+  # SNS認証ページ(APIでfacebookやtwitterアカウントでのログインを行う)
   def sns
 
   end
 
+  # お知らせ
   def notification
 
   end
 
+  # やることリスト
   def todo
 
   end
 
+  # 出品した商品 - 出品中
   def listing
 
   end
 
+  # 出品した商品 - 取引中
   def progress
 
   end
 
+  # 出品した商品 - 売却済み
   def completed
 
   end
 
+  # 購入した商品 - 取引中
+  def buying
+
+  end
+
+  # 購入した商品 - 過去の取引
   def pastransaction
 
   end
 
+  # ニュース一覧
   def news
 
   end
 
+  # 評価一覧
   def evaluates
 
   end
 
+  # お問い合わせ
   def support
 
   end
 
+  # ポイント
   def points
 
   end
 
+  # プロフィール
   def profile
 
   end
 
+  # 電話番号の確認
   def confirmation
 
   end
