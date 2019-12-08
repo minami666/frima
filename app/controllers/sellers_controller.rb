cda0cb16a5430a9d1cf1afa9578d9c9df74af91f
中里南 <minami.rage@gmail.com>
2019-12-08 22:55:31 +0900

    add : 出品の画面

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -1,2 +1,19 @@
 class SellersController < ApplicationController
+
+  # 商品の出品画面(もっとも難易度が高そうなので時間をかける)
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
 end
