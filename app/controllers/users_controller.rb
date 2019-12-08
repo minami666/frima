5b967662ade1e59547feb07d286996e896986d02
中里南 <minami.rage@gmail.com>
2019-12-08 22:44:43 +0900

    add : マイページC def〜end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,59 @@
 class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def sns
+
+  end
+
+  def notification
+
+  end
+
+  def todo
+
+  end
+
+  def listing
+
+  end
+
+  def progress
+
+  end
+
+  def completed
+
+  end
+
+  def pastransaction
+
+  end
+
+  def news
+
+  end
+
+  def evaluates
+
+  end
+
+  def support
+
+  end
+
+  def points
+
+  end
+
+  def profile
+
+  end
+
+  def confirmation
+
+  end
+
 end
