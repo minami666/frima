baf783b8a70147ee4299593e278f73b39d7fa147
中里南 <minami.rage@gmail.com>
2019-12-09 18:05:47 +0900

    fix : space

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,6 @@ class Category < ApplicationRecord
   validates :name, presence: true
 
   has_many :products
-  has_many :brands, through: :brand_categories
+  has_many :brands, through: :brand_categories
   has_many :brand_categories
 end
