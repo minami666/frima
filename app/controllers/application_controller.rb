97fe815bbd27b20e4749f89e4c5654d38ecfad4c
中里南 <minami.rage@gmail.com>
2019-12-09 19:29:18 +0900

    add : 本番環境のみbasic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :basic_auth
   before_action :authenticate_user!
@@ -11,6 +12,10 @@ class ApplicationController < ActionController::Base
   end
 
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
