0e242502f595bd83517fd19f266d8dbbd79c3050
中里南 <minami.rage@gmail.com>
2019-12-09 10:38:51 +0900

    fix : スペルミス

--- db/migrate/20191208111550_create_categories.rb
+++ db/migrate/20191208111550_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :categories do |t|
       t.string :name, null: false
-      t.sstring :text
+      t.string :text
       t.timestamps
     end
   end
