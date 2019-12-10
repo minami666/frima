7d25550f66c330277260ffa023966a210eea659e
中里南 <minami.rage@gmail.com>
2019-12-10 13:29:40 +0900

    fix : モデル名変更

--- app/models/seller_evaluate.rb
+++ app/models/seller_evaluate.rb
@@ -1,5 +1,5 @@
 class SellerEvaluate < ApplicationRecord
-  validates :sellers_id, presence: true
+  validates :seller_id, presence: true
 
   belongs_to :seller, foreign_key: true, dependent: :destroy
 end
