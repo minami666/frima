8a4ab6ba1898bbeff4f627eedfa3a949a6600b12
kantashimizu <gantaiqingshui968@gmail.com>
2019-12-10 13:30:37 +0900

    sell/画像

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -3,15 +3,18 @@ class SellersController < ApplicationController
   # 商品の出品画面(もっとも難易度が高そうなので時間をかける)
   def new
     @product = Product.new
+   # @product.productsimages.build
   end
 
   def create
-    @product = Product.new
-    if @product.save # 保存失敗して
+    @pro = Product.new(product_params)
+    if @pro.save
       redirect_to root_path
     else
-      binding.pry
+      #binding.pry
+      redirect_to "/products/show"
     end
+
   end
 
   def edit
@@ -24,6 +27,7 @@ class SellersController < ApplicationController
   end
   private
   def product_params
-    params.require(:product).permit(:name,:deliver_how,:deliver_day,:price,:explanation,:category_id,:state)
+    params.require(:product).permit(:name,:deliver_how,:deliver_day,:price,:explanation,:category_id,:state,
+    productsimages_attributes: [:id, :image, :_destroy])
   end
 end
--- app/models/productsimage.rb
+++ app/models/productsimage.rb
@@ -1,6 +1,5 @@
 class Productsimage < ApplicationRecord
   validates :products_id, presence: true
   validates :image, presence: true
-
-  belongs_to :product, foreign_key: true
+  belongs_to :products, foreign_key: true
 end
