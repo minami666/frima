ede556258984f44175a6dcf541296c2a2f37be81
中里南 <minami.rage@gmail.com>
2019-12-11 16:47:19 +0900

    add : フォーム

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_12_11_033026) do
-# ActiveRecord::Schema.define(version: 2019_12_10_065537) do
-# ActiveRecord::Schema.define(version: 2019_12_10_111718) do
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postnum", null: false
-    t.integer "prefecture", null: false
+    t.string "prefecture", null: false
     t.string "city", null: false
     t.integer "street_num", null: false
     t.string "building"
@@ -90,26 +86,17 @@ ActiveRecord::Schema.define(version: 2019_12_11_033026) do
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "deliver_how", null: false
-    t.integer "deliverday", null: false
+    t.integer "deliverday"
     t.integer "price", null: false
     t.string "explanation"
     t.integer "state", null: false
-    t.integer "category_id", null: false
-    t.integer "size_id", null: false
-    t.integer "brand_id", null: false
-    t.integer "user_id", null: false
-    t.integer "seller_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
-    t.integer "size_id"
-    t.integer "brand_id"
-    t.integer "user_id"
-    t.integer "seller_id"
   end
 
   create_table "productsimages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "product_id", null: false
+    t.integer "product_id"
     t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -133,7 +120,7 @@ ActiveRecord::Schema.define(version: 2019_12_11_033026) do
   end
 
   create_table "sellers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "product_id", null: false
+    t.integer "products_id", null: false
     t.integer "users_id", null: false
     t.integer "seller_evaluates_id", null: false
     t.datetime "created_at", null: false
