29fd2bf7f5e53ec297150716c29210033b41d869
takatakatakashi <iverson3kobe0824@gmil.com>
2019-12-11 13:02:51 +0900

    住所登録　都道府県

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_111718) do
+ActiveRecord::Schema.define(version: 2019_12_11_033026) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postnum", null: false
-    t.string "prefecture", null: false
+    t.integer "prefecture", null: false
     t.string "city", null: false
     t.integer "street_num", null: false
     t.string "building"
