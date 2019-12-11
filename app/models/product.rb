69fe40a0da7443fd2dd690fc4e8226ad5053d6d0
kantashimizu <gantaiqingshui968@gmail.com>
2019-12-11 10:17:27 +0900

    複数投稿

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,35 +6,32 @@ class Product < ApplicationRecord
     Tweet.where('name LIKE(?)', "%#{search}%")
   end
 
-validates :name, presence: true
-validates :deliver_how, presence: true
-validates :deliver_day, presence: true
-validates :price, presence: true
-validates :state, presence: true
+#validates :name, presence: true
+#validates :deliver_how, presence: true
+#validates :deliver_day, presence: true
+#validates :price, presence: true
+#validates :state, presence: true
   
 # #validates :brands_id, presence: true
 # #validates :sizes_id, presence: true
 # #validates :user_id, presence: true
 # #validates :sellers_id, presence: true
 
-
-# has_many :productsimages, dependent: :destroy #画像投稿用
-# accepts_nested_attributes_for :productsimages
-
-validates :brand_id, presence: true
-validates :size_id, presence: true
-validates :user_id, presence: true
-validates :seller_id, presence: true
+#validates :brand_id, presence: true
+#validates :size_id, presence: true
+#validates :user_id, presence: true
+#validates :seller_id, presence: true
 
 
 has_many :productsimages, dependent: :destroy
-has_many :productslikes, dependent: :destroy
+accepts_nested_attributes_for :productsimages 
+#has_many :productslikes, dependent: :destroy
 #belongs_to :user, foreign_key: true
 #belongs_to :size, foreign_key: true 
 #belongs_to :brand, foreign_key: true
 #belongs_to :category, foreign_key: true
-has_one :seller
-has_one :buyer
+#has_one :seller
+#has_one :buyer
 
 
 
