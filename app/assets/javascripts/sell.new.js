b04e47b844c9b33b97ece5d7aaa057bab3c247f5
中里南 <minami.rage@gmail.com>
2019-12-12 19:19:50 +0900

    Update sell.new.js

--- app/assets/javascripts/sell.new.js
+++ app/assets/javascripts/sell.new.js
@@ -3,7 +3,6 @@ $(function() {
 $(document).on('change','input[type= "file"].sell-upload',function(event) {
   var images = [];
   var inputs  =[];
-  console.log('ぱすお')
   var file = $(this).prop('files')[0];
   var reader = new FileReader();
   images.push($(this));
@@ -11,7 +10,6 @@ $(document).on('change','input[type= "file"].sell-upload',function(event) {
   reader.onload = function(e) {
     var btn_wrapper = $('<div class="btn_wrapper"><div class="btn edit">編集</div><div class="btn delete">削除</div></div>');
     $('.img').append(img);
-    //$('.img').append(btn_wrapper);
     img.find('img').attr({
       src: e.target.result
     })
@@ -22,7 +20,6 @@ $(document).on('change','input[type= "file"].sell-upload',function(event) {
 });
 
 $('#product_price').on('input',function() {
-  console.log('あぴぽ')
   var data = $('#product_price').val();
   var profit = Math.round(data * 0.9)
   var fee = (data - profit)
