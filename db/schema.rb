77752478e033695475fd4e5dbfc5f2036bb0aa2b
shota-tanabe <tnb.sot.19880906@gmail.com>
2019-12-12 18:00:34 +0900

    add : 商品画像11枚登録

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2019_12_11_033026) do
-
-#ActiveRecord::Schema.define(version: 2019_12_12_064015) do
-
+ActiveRecord::Schema.define(version: 2019_12_12_064015) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postnum", null: false
@@ -101,10 +97,7 @@ ActiveRecord::Schema.define(version: 2019_12_11_033026) do
     t.integer "seller_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
-    t.integer "category_id"
     t.integer "addresses_id"
-
   end
 
   create_table "productsimages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -145,15 +138,6 @@ ActiveRecord::Schema.define(version: 2019_12_11_033026) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -175,5 +159,4 @@ ActiveRecord::Schema.define(version: 2019_12_11_033026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "sns_credentials", "users"
 end
