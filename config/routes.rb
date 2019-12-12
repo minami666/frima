0f70b44a7f38392e64e709cdf382b462bd9db38e
中里南 <minami.rage@gmail.com>
2019-12-12 15:06:04 +0900

    fix : syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   #====================== デバイス ======================
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   controllers: {
     sessions: 'users/sessions',
     registrations: "users/registrations",
