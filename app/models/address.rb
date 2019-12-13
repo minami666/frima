86a19a037c76e8037f0613212b9a66128a815b09
中里南 <minami.rage@gmail.com>
2019-12-13 14:33:18 +0900

    add : 住所テーブル作り直し

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,11 +1,3 @@
 class Address < ApplicationRecord
-
-  validates :postnum, presence: true
-  validates :prefecture, presence: true
-  validates :city, presence: true
-  validates :street_num, presence: true
-  validates :user_id, presence: true
-
-  belongs_to :user
+  belongs_to :user
 end
-
