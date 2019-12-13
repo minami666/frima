e9f040e85d084c7a13d178d86dcd10e1d1dfefcf
中里南 <minami.rage@gmail.com>
2019-12-13 23:59:07 +0900

    add :

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,14 +2,16 @@ class ProductsController < ApplicationController
 
   def index
     @productsimage = Productsimage.all
-    @categorys = Category.all
+    @categories = Category.all
     @brands = Brand.all
   end
 
   def show
+    # @product = Product.find(id: params[:id])
+    @product = Product.find_by(id: 1)
   end
 
-  def search #検索機能用
+  def search
     @products = Product.search(params[:keyword])
   end
 
