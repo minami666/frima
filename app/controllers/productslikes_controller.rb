51dc972d9a33ad8f1ee2b526a791b536aff677a7
中里南 <minami.rage@gmail.com>
2019-12-14 15:42:49 +0900

    いいねボタン

--- app/controllers/productslikes_controller.rb
+++ app/controllers/productslikes_controller.rb
@@ -1,9 +1,11 @@
 class ProductslikesController < ApplicationController
 
   def new
+    @likes = Productslike.find(id: params[:id])
   end
 
   def create
+    @likes = Productslike.find(id: params[:id])
   end
 
   def edit
@@ -11,5 +13,11 @@ class ProductslikesController < ApplicationController
 
   def update
   end
+
+  private
+
+    def likes_params
+      params.require(:productslike).permit(:products_likes)
+    end
 
 end
