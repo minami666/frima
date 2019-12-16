676fcdc01c8f1ca16f027302de73d5c7404ecdf2
中里南 <minami.rage@gmail.com>
2019-12-16 13:13:49 +0900

    add : sellers

---
+++ app/assets/javascripts/sellers.js
@@ -0,0 +1,32 @@
+$(function() {
+
+$(document).on('change','input[type= "file"].sell-upload',function(event) {
+  var images = [];
+  var inputs  =[];
+  var file = $(this).prop('files')[0];
+  var reader = new FileReader();
+  images.push($(this));
+  var img = $(`<div class= "img_view"><img></div>`);
+  reader.onload = function(e) {
+    var btn_wrapper = $('<div class="btn_wrapper"><div class="btn edit">編集</div><div class="btn delete">削除</div></div>');
+    $('.img').append(img);
+    img.find('img').attr({
+      src: e.target.result
+    })
+  }
+  reader.readAsDataURL(file);
+  inputs.push(img);
+
+});
+
+$('#product_price').on('input',function() {
+  var data = $('#product_price').val();
+  var profit = Math.round(data * 0.9)
+  var fee = (data - profit)
+  $('.sell-margin_right').html(fee)
+  $('.sell-margin_right').prepend('¥')
+  $('.sell-margin_right2').html(profit)
+  $('.sell-margin_right2').prepend('¥')
+  })
+});
+
