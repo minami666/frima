fb8c6c9b31f4c7be80d7639d2a4221ba00a719d8
Ota80 <uota80@gmail.com>
2019-12-16 18:20:06 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -130,15 +130,6 @@ ActiveRecord::Schema.define(version: 2019_12_13_052436) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -161,5 +152,4 @@ ActiveRecord::Schema.define(version: 2019_12_13_052436) do
   end
 
   add_foreign_key "addresses", "users"
-  add_foreign_key "sns_credentials", "users"
 end
