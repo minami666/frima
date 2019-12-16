b6702d4f9cc93b653f3a53cbffc9a7dd3fa2d019
takatakatakashi <iverson3kobe0824@gmil.com>
2019-12-16 20:45:16 +0900

    mirate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-# ActiveRecord::Schema.define(version: 2019_12_13_114514) do
-# ActiveRecord::Schema.define(version: 2019_12_13_052436) do
-
 ActiveRecord::Schema.define(version: 2019_12_16_090447) do
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postnum", null: false
@@ -106,7 +101,7 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
   end
 
   create_table "productsimages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "product_id"
+    t.integer "products_id", null: false
     t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -144,15 +139,6 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -179,5 +165,4 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
   add_foreign_key "sellers", "products"
   add_foreign_key "sellers", "seller_evaluates", column: "seller_evaluates_id"
   add_foreign_key "sellers", "users"
-  add_foreign_key "sns_credentials", "users"
 end
