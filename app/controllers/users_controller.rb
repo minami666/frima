156304c2880381e48c8d5dd4f1fd2a1301dbbe12
Ota80 <uota80@gmail.com>
2019-12-16 17:32:15 +0900

    コメントアウトの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,2 @@
 class UsersController < ApplicationController
-  # binding.pry
-
-  # def new
-  #   @user = User.new
-  # end
-
-  #   binding.pry
-
-  # def create
-  #   @user = User.new
-  #   if @user.save
-  #     redirect_to '/addresses/new'
-  #   end
-  # end
-
-  # private
-
 end
