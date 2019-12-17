762089f79eba190e66964924f7572f644db39e42
takatakatakashi <iverson3kobe0824@gmil.com>
2019-12-17 12:12:28 +0900

    クレカ　修正

--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,4 +1,5 @@
 class PurchaseController < ApplicationController
+  before_action :set_item,only: [:index, :payjp]
 
   def index
     card = Card.where(user_id: current_user.id).first
@@ -7,7 +8,7 @@ class PurchaseController < ApplicationController
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
-      Payjp.api_key = "sk_test_a3ad683fae92739356cf5902"
+      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
       #保管した顧客IDでpayjpから情報取得
       customer = Payjp::Customer.retrieve(card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
@@ -17,7 +18,7 @@ class PurchaseController < ApplicationController
 
   def pay
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = "sk_test_a3ad683fae92739356cf5902"
+    Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
     Payjp::Charge.create(
     :amount => 135000, #支払金額を入力（itemテーブル等に紐づけても良い）
     :customer => card.customer_id, #顧客ID
@@ -27,4 +28,9 @@ class PurchaseController < ApplicationController
     redirect_to action: 'done' #完了画面に移動
   end
 
+end
+
+private
+def set_item
+  @card = Card.find_by(user_id: current_user.id)
 end
