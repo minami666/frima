96df3d6b5de7e96f2cf811b3d87bf52a46ee128d
中里南 <minami.rage@gmail.com>
2019-12-17 17:48:20 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise  :omniauthable, omniauth_providers: %i[facebook google_oauth2],
-          :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :validatable
+
+  devise :omniauthable,omniauth_providers: %i[facebook google_oauth2],:database_authenticatable,:registerable,:recoverable,:rememberable,:validatable
 
   mount_uploader :image, ImageUploader
 
