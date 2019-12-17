28ff3d87b7182c3a9e152d0e04b2749dc571b242
shota-tanabe <tnb.sot.19880906@gmail.com>
2019-12-17 14:36:18 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -87,18 +87,22 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "deliver_how", null: false
-    t.integer "deliverday"
+    t.integer "deliverday", null: false
     t.integer "price", null: false
     t.string "explanation"
     t.integer "state", null: false
+    t.integer "category_id", null: false
+    t.integer "size_id", null: false
+    t.integer "brand_id", null: false
+    t.integer "user_id", null: false
+    t.integer "seller_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "category_id"
     t.integer "addresses_id"
   end
 
   create_table "productsimages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "product_id"
+    t.integer "product_id", null: false
     t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -136,15 +140,6 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -171,5 +166,4 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
   add_foreign_key "sellers", "products"
   add_foreign_key "sellers", "seller_evaluates", column: "seller_evaluates_id"
   add_foreign_key "sellers", "users"
-  add_foreign_key "sns_credentials", "users"
 end
