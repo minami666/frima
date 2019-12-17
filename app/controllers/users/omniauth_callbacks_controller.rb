da04128316ed336a2a560b4ac85fdc0a82174a1b
中里南 <minami.rage@gmail.com>
2019-12-17 20:01:57 +0900

    fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,46 +1,21 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # callback for facebook
   def facebook
-    callback_for(:facebook)
+    callback_from :facebook
   end
 
-  # callback for google
-  def google_oauth2
-    callback_for(:google)
-  end
+  private
 
-  # common callback method
-  def callback_for(provider)
-    info = User.find_oauth(request.env["omniauth.auth"])
-    # snsの情報からuserが登録されているか　or snsから情報を取得できているかを確認
-    @user = User.where(nickname: info[:user][:nickname]).or(User.where(email: info[:user][:email])).first || info[:user]
+  def callback_from(provider)
+    provider = provider.to_s
 
-    # persisted?はデータがDBに保存されているかを確認する/配列に対しては使えないから@userを定義するときは気をつける
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+
     if @user.persisted?
-      #保存されていればログインしてroot_pathにリダイレクトされる
+      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
-      # 登録するアクションに取得した値を渡すために。sessionを利用してuserインスタンスを作成する
-      session[:nickname] = info[:user][:nickname]
-      session[:email] = info[:user][:email]
-
-      #snsでのユーザ登録ではパスワードを入力させないので準備する。パスワードを作成するDeviseメソッドもある。
-      #今回のバリデーションは英数字のみなのでこっちを採用
-      session[:password_confirmation] = SecureRandom.alphanumeric(30)
-
-      #SnsCredentialが登録されていないとき
-      if SnsCredential.find_by(uid: info[:sns][:uid], provider: info[:sns][:provider]).nil?
-        #ユーザ登録と同時にsns_credentialも登録するために
-        session[:uid] = info[:sns][:uid]
-        session[:provider] = info[:sns][:provider]
-      end
-      #登録フォームのviewにリダイレクトさせる
-      redirect_to step1_signups_path
+      session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      redirect_to new_user_registration_url
     end
-  end
-
-  def failure
-    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
 
          mount_uploader :image, ImageUploader
@@ -21,58 +21,19 @@ class User < ApplicationRecord
   belongs_to_active_hash :day
 
 
-#omniauth_callbacks_controllerで呼び出すメソッド
-def self.find_oauth(auth)
-  uid = auth.uid
-  provider = auth.provider
-  snscredential = SnsCredential.where(uid: uid, provider: provider).first #firstをつけないとデータが配列で返されて使いたいメソッドが使えなくて困る
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
 
-  #sns_credentialsが登録されている
-  if snscredential.present?
-    user = User.where(email: auth.info.email).first
-
-    # userが登録されていない
-    unless user.present?
-      user = User.new(
-      nickname: auth.info.name,
-      email: auth.info.email,
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    auth.info.email,
+        password: Devise.friendly_token[0, 20]
       )
     end
-    sns = snscredential
-    #返り値をハッシュにして扱いやすくする
-    #活用例 info = User.find_oauth(auth)
-    #session[:nickname] = info[:user][:nickname]
-    { user: user, sns: sns}
 
-  #sns_credentialsが登録されていない
-  else
-    user = User.where(email: auth.info.email).first
-
-
-    # userが登録されている
-    if user.present?
-      sns = SnsCredential.create(
-        uid: uid,
-        provider: provider,
-        user_id: user.id
-      )
-
-      { user: user, sns: sns}
-
-    # userが登録されていない
-    else
-      user = User.new(
-      nickname: auth.info.name,
-      email: auth.info.email,
-      )
-      sns = SnsCredential.new(
-        uid: uid,
-        provider: provider
-      )
-
-      { user: user, sns: sns}
-        end
-    end
+    user
   end
 
 
