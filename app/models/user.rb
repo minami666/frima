ef8eefb52233213cef8918c53e04da500bcbe4df
中里南 <minami.rage@gmail.com>
2019-12-17 21:34:38 +0900

    remove : 無駄コメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,6 @@ class User < ApplicationRecord
 
          mount_uploader :image, ImageUploader
 
-  # validates :nickname, presence: true,unique: true
-  # validates :tel, presence: true,unique: true
-  # validates :family_name_knj, presence: true
-  # validates :family_name_ktkne, presence: true
-  # validates :first_name_knj, presence: true
-  # validates :first_name_ktkne, presence: true
-  # validates :birth, presence: true
-
   has_many :products
   has_many :productslikes
   has_many :messages
