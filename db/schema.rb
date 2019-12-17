3edd6ad55e95b515de798808d27e097aad83fccc
中里南 <minami.rage@gmail.com>
2019-12-17 15:27:43 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -74,6 +74,15 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "number", null: false
+    t.string "name", null: false
+    t.integer "deadline", null: false
+    t.integer "security_num", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "text", null: false
     t.integer "products_id", null: false
@@ -101,7 +110,7 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
   end
 
   create_table "productsimages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "products_id", null: false
+    t.integer "product_id"
     t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -139,6 +148,15 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -165,4 +183,5 @@ ActiveRecord::Schema.define(version: 2019_12_16_090447) do
   add_foreign_key "sellers", "products"
   add_foreign_key "sellers", "seller_evaluates", column: "seller_evaluates_id"
   add_foreign_key "sellers", "users"
+  add_foreign_key "sns_credentials", "users"
 end
