456d9b82588ee595b531551c624d155d532bd905
中里南 <minami.rage@gmail.com>
2019-12-21 00:21:02 +0900

    fix

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -58,14 +58,12 @@ class SellersController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:name,:explanation,:category_id,:brand_id,:state,:size_id,:deliver_how,
-    :deliverday,:price,:addresses_id,productsimages_attributes: [:image])
-    .merge(user_id: current_user.id,seller_id: current_user.id)
+    :deliverday,:price,:addresses_id,productsimages_attributes: [:image]).merge(user_id: current_user.id,seller_id: current_user.id)
   end
 
   def product_params2
     params.require(:product).permit(:name,:explanation,:category_id,:brand_id,:state,:size_id,:deliver_how,
-    :deliverday,:price,:addresses_id)
-    .merge(user_id: current_user.id,seller_id: current_user.id)
+    :deliverday,:price,:addresses_id).merge(user_id: current_user.id,seller_id: current_user.id)
   end
 
 
