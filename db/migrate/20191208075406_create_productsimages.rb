d3db532db843d15f44185ad6edc1664aa8661a1d
Ota80 <uota80@gmail.com>
2019-12-20 15:19:58 +0900

    fix

--- db/migrate/20191208075406_create_productsimages.rb
+++ db/migrate/20191208075406_create_productsimages.rb
@@ -1,7 +1,7 @@
 class CreateProductsimages < ActiveRecord::Migration[5.2]
   def change
     create_table :productsimages do |t|
-      t.references : :product, foreign_key: true
+      t.references :product, foreign_key: true
       t.string :image,  null: false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_20_033306) do
-
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "postnum", null: false
-    t.integer "prefecture", null: false
-    t.string "city", null: false
-    t.integer "street_num", null: false
-    t.string "building"
-    t.string "family_name_knj"
-    t.string "first_name_knj"
-    t.string "family_name_ktkn"
-    t.string "first_name_ktkn"
-    t.string "tel"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_addresses_on_user_id"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
   create_table "brand_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "categories_id"
     t.integer "brands_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "brand_name", null: false
-    t.string "text", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -52,35 +28,6 @@ ActiveRecord::Schema.define(version: 2019_12_20_033306) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "buyers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "products_id", null: false
-    t.integer "users_id", null: false
-    t.integer "buyer_evaluates_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "customer_id"
-    t.integer "number"
-    t.integer "deadline"
-    t.integer "security_num"
-    t.bigint "user_id", null: false
-    t.bigint "card_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["card_id"], name: "index_cards_on_card_id"
-    t.index ["user_id"], name: "index_cards_on_user_id"
-  end
-
-  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "number", null: false
     t.string "name", null: false
@@ -88,38 +35,6 @@ ActiveRecord::Schema.define(version: 2019_12_20_033306) do
     t.integer "security_num", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "text", null: false
-    t.integer "products_id", null: false
-    t.integer "users_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "deliver_how", null: false
-    t.integer "deliverday"
-    t.integer "price", null: false
-    t.string "explanation"
-    t.integer "state", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "category_id"
-    t.integer "size_id"
-    t.integer "brand_id"
-    t.integer "user_id"
-    t.integer "seller_id"
-    t.integer "addresses_id"
-  end
-
-  create_table "productsimages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "product_id"
-    t.string "image", default: ""
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "productslikes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -139,52 +54,4 @@ ActiveRecord::Schema.define(version: 2019_12_20_033306) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "sellers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "product_id"
-    t.index ["product_id"], name: "index_sellers_on_product_id"
-    t.index ["user_id"], name: "index_sellers_on_user_id"
-  end
-
-  create_table "sizes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "size_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "nickname", null: false
-    t.string "image"
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "family_name_knj"
-    t.string "family_name_ktkn"
-    t.string "first_name_knj"
-    t.string "first_name_ktkn"
-    t.string "birth"
-    t.string "tel"
-    t.string "profile"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  add_foreign_key "addresses", "users"
-  add_foreign_key "cards", "cards"
-  add_foreign_key "sellers", "products"
-  add_foreign_key "sellers", "users"
-  add_foreign_key "sns_credentials", "users"
 end
