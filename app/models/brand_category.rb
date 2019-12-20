d9a447c5180286160b06ae32da759484e7287630
中里南 <minami.rage@gmail.com>
2019-12-20 15:14:31 +0900

    remove

--- app/models/brand_category.rb
+++
@@ -1,6 +0,0 @@
-class BrandCategory < ApplicationRecord
-
-  belongs_to :category, foreign_key: true
-  belongs_to :brand, foreign_key: true
-
-end
--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,6 @@ class Message < ApplicationRecord
   # validates :product_id, presence: true
   # validates :user_id, presence: true
 
-   belongs_to :user, foreign_key: true
-   belongs_to :product, foreign_key: true
+  belongs_to :user, foreign_key: true
+  belongs_to :product, foreign_key: true
 end
