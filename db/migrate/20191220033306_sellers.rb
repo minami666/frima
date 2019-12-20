f724256a8389fc0232ade8df96ba089aa80eca7f
中里南 <minami.rage@gmail.com>
2019-12-20 19:20:00 +0900

    add

--- db/migrate/20191220033306_sellers.rb
+++ db/migrate/20191220033306_sellers.rb
@@ -1,8 +1,8 @@
-# class Sellers < ActiveRecord::Migration[5.2]
-#   def change
-#     create_table :sellers do |t|
-#       t.references :user,foreign_key: true
-#       t.references :product,foreign_key: true
-#     end
-#   end
-# end
+class Sellers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sellers do |t|
+      t.references :user,foreign_key: true
+      t.references :product,foreign_key: true
+    end
+  end
+end
