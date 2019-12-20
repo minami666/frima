78be547b6365cfedd2b4979da802d0cbfc8ae1f3
中里南 <minami.rage@gmail.com>
2019-12-20 16:46:17 +0900

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ApplicationRecord
           nickname: auth.info.name,
           email:    auth.info.email,
           password: Devise.friendly_token[0, 20],
-          telephone: "08000000000"
+          # telephone: "08000000000"
           )
         SnsCredential.create(
           uid: uid,
