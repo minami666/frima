186cf1ec68c9e082014032b780cadd7328b5c15a
中里南 <minami.rage@gmail.com>
2019-12-22 20:49:24 +0900

    add : 顧客登録

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,50 +1,38 @@
 class CardController < ApplicationController
 
   require "payjp"
+  before_action :set_card
 
-  def new
-    card = Card.where(user_id: current_user.id)
-    redirect_to action: "show" if card.exists?
+  def new # カードの登録画面。送信ボタンを押すとcreateアクションへ。
+    card = Card.where(user_id: current_user.id).first
+    redirect_to action: "index" if card.present?
   end
 
-  def pay #payjpとCardのデータベース作成を実施します。
+  # indexアクションはここでは省略
+
+  def create #PayjpとCardのデータベースを作成
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+
     if params['payjp-token'].blank?
       redirect_to action: "new"
     else
+      # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
       customer = Payjp::Customer.create(
-      card: params['payjp-token'],
-      currency: 'jpy',
-      ) #念の為metadataにuser_idを入れましたがなくてもOK
+        description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
+        card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
+      )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
-        redirect_to action: "show"
+        redirect_to action: "index"
       else
-        redirect_to action: "pay"
+        redirect_to action: "create"
       end
     end
   end
 
-  def delete #PayjpとCardデータベースを削除します
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-    else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      customer.delete
-      card.delete
-    end
-      redirect_to action: "new"
-  end
+  private
 
-  def show #Cardのデータpayjpに送り情報を取り出します
-    card = Card.where(user_id: current_user.id).first
-    if card.blank?
-      redirect_to action: "new"
-    else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
-    end
+  def set_card
+    @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
   end
 end
