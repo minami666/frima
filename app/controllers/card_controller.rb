3dedeb8d6fdbe5c330edfa20d8512097081bad5e
中里南 <minami.rage@gmail.com>
2019-12-22 21:21:57 +0900

    add

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -19,7 +19,9 @@ class CardController < ApplicationController
       # トークンが正常に発行されていたら、顧客情報をPAY.JPに登録します。
       customer = Payjp::Customer.create(
         description: 'test', # 無くてもOK。PAY.JPの顧客情報に表示する概要です。
+        # email: current_user.email,
         card: params['payjp-token'], # 直前のnewアクションで発行され、送られてくるトークンをここで顧客に紐付けて永久保存します。
+        # metadata: {user_id: current_user.id} # 無くてもOK。
       )
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
