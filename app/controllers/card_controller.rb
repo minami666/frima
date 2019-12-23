476dd1502c23cb32e3fb92de7c65352c5388d268
minami666 <55865498+minami666@users.noreply.github.com>
2019-12-23 15:07:58 +0900

    Revert "Revert "【バックエンド】クレジットカード実装：データベース保存""

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -4,7 +4,9 @@ class CardController < ApplicationController
   require "payjp"
 
   def new
-    @card = Card.new
+    # @card = Card.new
+    card = Card.where(user_id: current_user.id)
+    redirect_to action: "show" if card.exists?
   end
 
   def pay #payjpとCardのデータベース作成を実施します。
@@ -17,14 +19,27 @@ class CardController < ApplicationController
       metadata: {user_id: current_user.id}
       ) #念の為metadataにuser_idを入れましたがなくてもOK
       @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save!
-        redirect_to action: "show"
+
+      if @card.save
+        redirect_to mypages_done_path
+        
+
       else
         redirect_to action: "pay"
       end
     end
   end
-
+  
+  def show #Cardのデータpayjpに送り情報を取り出します
+    if @card.blank?
+      redirect_to action: "new"
+    else
+      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      customer = Payjp::Customer.retrieve(card.customer_id)
+      @default_card_information = customer.cards.retrieve(card.card_id)
+    end
+  end
+  
   def delete #PayjpとCardデータベースを削除します
     if @card.blank?
       redirect_to controller: "card", action: "new"
@@ -37,19 +52,9 @@ class CardController < ApplicationController
       redirect_to action: "new"
   end
 
-  def show #Cardのデータpayjpに送り情報を取り出します
-    if card.blank?
-      redirect_to action: "new"
-    else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer = Payjp::Customer.retrieve(card.customer_id)
-      @default_card_information = customer.cards.retrieve(card.card_id)
-      redirect_to product: "index"
-    end
-  end
 end
 
 private
 def set_card
-  @card = Card.find_by(user_id: current_user.id).first
+  @card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
 end
