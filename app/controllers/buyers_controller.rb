83beecf20345171777dda7567eefdf9b6a73b89f
中里南 <minami.rage@gmail.com>
2019-12-23 20:39:48 +0900

    fix

--- app/controllers/buyers_controller.rb
+++ app/controllers/buyers_controller.rb
@@ -1,5 +1,4 @@
 class BuyersController < ApplicationController
-  before_action :set_card, :set_product
 
   def new
     @product = Product.find(params[:product_id])
@@ -12,7 +11,7 @@ class BuyersController < ApplicationController
     if @card.blank?
       redirect_to new_card_path
     else
-      Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
+      Payjp.api_key = 'sk_test_a3ad683fae92739356cf5902'
       customer = Payjp::Customer.retrieve(@card.customer_id)
       @default_card_information = customer.cards.retrieve(@card.card_id)
     end
@@ -21,14 +20,16 @@ class BuyersController < ApplicationController
   def create
     @buyer = Buyer.new(buy_params)
     if @buyer.save
+      card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
+      product = Product.find(params[:product_id])
       amount = product.price
-      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp.api_key = 'sk_test_a3ad683fae92739356cf5902'
       Payjp::Charge.create(
         :amount => amount, #支払金額を入力（itemテーブル等に紐づけても良い）
         :customer => card.customer_id, #顧客ID
         :currency => 'jpy', #日本円
       )
-      
+
       redirect_to root_path
     else
       redirect_to new_product_buyer_path
@@ -43,15 +44,6 @@ class BuyersController < ApplicationController
 
   def buy_params
     params.require(:buyer).permit!
-  end
-
-  private
-  def set_card
-    card = Card.where(user_id: current_user.id).first if Card.where(user_id: current_user.id).present?
-  end
-
-  def set_product
-    product = Product.find(params[:product_id])
   end
 
 end
